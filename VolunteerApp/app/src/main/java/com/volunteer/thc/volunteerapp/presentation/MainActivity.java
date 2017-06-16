c5c507584a23704d7a9c11b3403cbc2de8a1181a
George Rosescu <george.rosescu@gmail.com>
2017-06-16 19:00:19 +0300

    nav menu items, main activity switch between menu items

--- VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/presentation/MainActivity.java
+++ VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/presentation/MainActivity.java
@@ -114,17 +114,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_events) {
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_logout){
             Auth.signOut();
