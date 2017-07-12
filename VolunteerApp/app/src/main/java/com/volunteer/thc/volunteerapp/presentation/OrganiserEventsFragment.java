34b71060b184ab2fbaf2fdd1373fc3e3b3641407
PaulPop <poppaultudor@gmail.com>
2017-07-12 19:04:15 +0300

    added RecyclerView adapter settings in OrganiserEventsFragment

--- VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/presentation/OrganiserEventsFragment.java
+++ VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/presentation/OrganiserEventsFragment.java
@@ -2,27 +2,33 @@ package com.volunteer.thc.volunteerapp.presentation;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.volunteer.thc.volunteerapp.R;
+import com.volunteer.thc.volunteerapp.adaptor.OrgEventsAdaptor;
+import com.volunteer.thc.volunteerapp.model.Event;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Cristi on 6/17/2017.
  */
 
-public class OrganiserEventsFragment extends Fragment{
+public class OrganiserEventsFragment extends Fragment {
 
     private FloatingActionButton mAddEvent;
     private FragmentTransaction mFragmentTransaction;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                    Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_organiserevents, container, false);
 
@@ -38,7 +44,14 @@ public class OrganiserEventsFragment extends Fragment{
             }
         });
 
-        //TODO: retrieve current organiser's events
+        List<Event> EventsList = new ArrayList<>();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.RecViewOrgEvents);
+        recyclerView.setHasFixedSize(true);
+        OrgEventsAdaptor adapter = new OrgEventsAdaptor(EventsList);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
 
         return view;
     }
