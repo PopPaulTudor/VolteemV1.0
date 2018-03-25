9a83dc9ccbfe92fd46d8e1dbc4824c10571cae4a
Vlad Iliescu <viliescu2000@gmail.com>
2018-03-25 18:45:45 +0300

    Bug fix: on Oreo devices, don't start firebase service on boot

--- VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/notification/StartFirebaseServiceAtBoot.java
+++ VolunteerApp/app/src/main/java/com/volunteer/thc/volunteerapp/notification/StartFirebaseServiceAtBoot.java
@@ -3,6 +3,7 @@ package com.volunteer.thc.volunteerapp.notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 /**
  * Created by Cristi on 8/26/2017.
@@ -11,6 +12,8 @@ import android.content.Intent;
 public class StartFirebaseServiceAtBoot extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, FirebaseNewsService.class));
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            context.startService(new Intent(context, FirebaseNewsService.class));
+        }
     }
 }
